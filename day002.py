fc90ae8918df6efbcc383e70475b500cdc7d765a
leyo2001 <leyo2001@naver.com>
2024-01-15 11:51:55 +0900

    v0.6 day002

--- day002.py
+++ day002.py
@@ -1,14 +1,11 @@
-# #x = 2
-# y = x + 5 #name 'x' is not defined
-# print(y)
+base_number = int(input('Input base number : '))
+exponent_number = int(input('Input exponent number : '))
+print(f'밑은 {base_number}. 지수는{exponent_number}, 결과 값은{base_number**exponent_number}')
 
-print(type(3.14))
-print(type(3.14) == float)
-print(isinstance(3.14, float))
-print(isinstance(7, int))
-
-artists = ['BTS' , '뉴진스', '핑클' , 'SES' , 'HOT' , '블랙핑크']
-group = artists
-print(artists , group)
-artists[2] = '세븐틴'
-print(artists , group)
+# money = 5,000,000
+# print(money)
+# print(type(money))
+#
+# cash = 5_000_000
+# print(cash)
+# print(type(cash))
