0b7bc944ca5391387548fd036b9573e404251a2c
leyo2001 <leyo2001@naver.com>
2024-01-15 11:43:37 +0900

    v0.5 day002

--- day002.py
+++ day002.py
@@ -1,20 +1,14 @@
-# a = "Inha University"
-# a[5]="u"
-# error
+# #x = 2
+# y = x + 5 #name 'x' is not defined
+# print(y)
 
+print(type(3.14))
+print(type(3.14) == float)
+print(isinstance(3.14, float))
+print(isinstance(7, int))
 
-#ok
-#case-sensitive
-abc = 8
-Abc = 9
-ABC = 6
-print(abc,Abc,ABC)
-
-test9 = 77
-#9test = 77
-_test = 71
-
-#False = 123 #reserve word
-
-
-
+artists = ['BTS' , '뉴진스', '핑클' , 'SES' , 'HOT' , '블랙핑크']
+group = artists
+print(artists , group)
+artists[2] = '세븐틴'
+print(artists , group)
