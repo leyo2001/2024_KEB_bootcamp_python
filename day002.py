b3501d15e9e4b2ddc4e707b14774c4466468f1a1
leyo2001 <leyo2001@naver.com>
2024-01-15 14:58:56 +0900

    v1.1 day002

--- day002.py
+++ day002.py
@@ -1,3 +1,10 @@
-# (100℉ -32) * 5/9 = 37.778 ℃
-fahrenheit = float(input('Input Fahrenheit : '))
-print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32) * 5.0/9.0):.4f}C')
+menu = input("1) Fahrenheit - Celsius   2)Celsius -> Fahrenheit     3) Quit prograrm")
+
+if menu == '1':
+    fahrenheit = float(input('Input Fahrenheit : '))
+    print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32)*5.0/9.0):.4f}C')
+
+
+elif menu =='2':
+    celsius = float(input('Input Celcius : '))
+    print(f'Celsius : {celsius}C, Fahrenheit: {((celsius*0.0/5.0)+32.0):.4f}F')
