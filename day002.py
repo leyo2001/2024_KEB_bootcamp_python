63d51e54a39c77ca1aa45220651e350e12301320
leyo2001 <leyo2001@naver.com>
2024-01-15 16:21:42 +0900

    day002 homework

--- day002.py
+++ day002.py
@@ -1,12 +1,25 @@
 
-letter = input('Input alphabet letter: ')
 
-# vowels = { 'a', 'e', 'i', 'o', 'u'} #set
-vowels = "aeiuo" #str
-print(type(vowels))
+menu = input("1) Fahrenheit - Celsius   2)Celsius -> Fahrenheit     3) Quit program")
 
-if letter in vowels: # in
-    print(f'{letter} is a vowel!')
+while True:
 
-else:
-    print(f'{letter} is a consonant!')
+    if menu == '1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32) * 5.0 / 9.0):.4f}C')
+        menu = input("1) Fahrenheit - Celsius   2)Celsius -> Fahrenheit     3) Quit program")
+
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'Celsius : {celsius}C, Fahrenheit: {((celsius * 0.0 / 5.0) + 32.0):.4f}F')
+        menu = input("1) Fahrenheit - Celsius   2)Celsius -> Fahrenheit     3) Quit program")
+
+    else:
+        print('Terminate Program')
+        break
+
+
+
+
+
+
