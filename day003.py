f24c40ffe5f98e9be3c4a4f91fa4ae1f77f63138
leyo2001 <leyo2001@naver.com>
2024-01-16 10:35:12 +0900

    v1.4  day003

---
+++ day003.py
@@ -0,0 +1,16 @@
+university = 'Inha\nUniversity'
+# university = r'Inha\nUniversity'
+print(university[:4])
+print(university[:-11])
+print(university[0:len(university)])
+print(university[:16])
+print(university[::2])
+
+
+number1 = input("First number : ")
+number2 = input("Second number : ")
+print(number1 + number2) #concatenation
+
+print(number1 * 3)
+
+print(university[0:1])
