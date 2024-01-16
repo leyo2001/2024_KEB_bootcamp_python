96e7bba52ab0cfaedff4174b2fc05a57602a7ff5
leyo2001 <leyo2001@naver.com>
2024-01-16 15:02:52 +0900

    v2.6 prime number  day003

--- day003.py
+++ day003.py
@@ -1,17 +1,23 @@
-univ = "inha"
-i = 0
-while i < len(univ):
-    print(univ[i],end=' ')
-    i = i+1
+#prime number
+number = int(input("Input number : "))
+is_prime=True
 
-print()
+if number < 2:
+    print(f'{number} is NOT prime number')
+else:
+    i=2
+    for i in range(2,number):
+            is_prime = False # remove +
+            break
 
-for letter in univ:
-    print(letter, end=' ')
 
-print()
+    if is_prime:
+        print(f'{number} is prime number')
+    else:
+        print(f'{number} is NOT prime number')
 
-# for k in range(0,len(univ),1):
-# for k in range(0, len(univ)):
-for k in range(len(univ)):
-    print(univ[k], end=' ')
+
+
+# subjects = { 'python' : 'kim', 'c++' : 'sung', 'datastructure' : 'kim', ' database': 'kang'}
+# print("{0[python]} {0[c++]}".format(subjects))
+
