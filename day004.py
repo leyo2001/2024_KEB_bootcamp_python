1d6dd19b78ca8dd6cccb58905e16e75c1921a7ec
leyo2001 <leyo2001@naver.com>
2024-01-17 13:04:11 +0900

    v3.4 day04

--- day004.py
+++ day004.py
@@ -1,28 +1,12 @@
-t1 = (5)
-t2 = 5,
-t3 = (5,)
-t4 = (5,7)
-t5 = 5,7
-print(type(t1),type(t2),type(t3),type(t4),type(t5))
-t6 = 'python' , 'kim' # packing
-print(type(t6), t6[1])
-subject, prof = t6 # unpacking
-# a, b, c = t6 # ValueError: not enough values to unpack (expected 3, got 2)
-print(prof)
-print(subject)
-t7 = ()
-t8 = tuple()
-print(type(t7), type(t8), type(9,), type((9,)))
-t9 = 1,2,3
-t10 = 1,2
-print(t9 == t10)
-print(t9 <= t10)
-print(t9 > t10)
-t11 = 4.43,
-t12 = 3.98, 4.1, 3.27
-# print(t11 + t12)
-print(id(t11))
-t11 = t11 + t12 #t11 +=t12
-print(id(t11))
-print(t11)
-
+subjects = ["c++", "5", "java", "python", "java", "9", "데이터베이스"]
+#print(subjects[::-1])
+#subjects[::-1]
+# subjects = subjects[::-1] #subjects.reverse()
+# print(subjects)
+# subjects.remove('java')
+# del subjects[1]
+# subjects.pop(1)
+# subjects.sort(reverse=True)
+copy_subjects = sorted(subjects)
+print(subjects)
+print(copy_subjects)
