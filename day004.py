7314cb2d04a6019826281927a24138ef8dfe24be
leyo2001 <leyo2001@naver.com>
2024-01-17 17:56:46 +0900

    v4.2 day04 homework

--- day004.py
+++ day004.py
@@ -1,37 +1,135 @@
-import random
-drinks_foods = {"위스키": "초콜릿", "와인": "치즈", "소주": "삽겹살", "고량주": "양꼬치"}
-# print(drinks_foods)
-# print(drinks_foods.pop("고량주"))
-# print(drinks_foods)
+# 8.1
+# e2f = dict(dog = 'chien', cat = 'chat', walrus = 'morse')
+# print(e2f)
 
-#del drinks_foods["위스키"]
-#drinks_foods["사케"] = "광어회"
-japan_drinks_foods = {"사케": "광어회", "위스키": "낙곱새"}
-drinks_foods.update(japan_drinks_foods)
+# 8.2
+# e2f = { 'dog' : 'chien', 'cat' : 'chat', 'walrus' : 'morse'}
+# print(e2f['walrus'])
 
-#drink = input(drinks_foods.keys())
-drinks_foods_keys = list(drinks_foods)
-# print(drinks_foods_keys)
-# #print(drinks_foods_keys.pop(0))
-# print(drinks_foods_keys.remove("위스키"))
-# print(drinks_foods_keys)
-#print(random.choice(drinks_foods_keys))
+# 8.3
+# e2f = { 'dog' : 'chien', 'cat' : 'chat', 'walrus' : 'morse'}
+# f2e = {}
+# for key, value in e2f.items():
+#     f2e[value] = key
+#
+# print(f2e)
 
-while True:
-    menu = input(f'다음 술중에 고르세요.\n1) {drinks_foods_keys[0]}   2) {drinks_foods_keys[1]}   3) {drinks_foods_keys[2]}   4) {drinks_foods_keys[3]}   5) {drinks_foods_keys[4]}   6) 아무거나   7) 종료 : ')
-    if menu == '1':
-        print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]} 입니다')
-    elif menu == '2':
-        print(f'{drinks_foods_keys[1]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[1]]} 입니다')
-    elif menu == '3':
-        print(f'{drinks_foods_keys[2]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[2]]} 입니다')
-    elif menu == '4':
-        print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]} 입니다')
-    elif menu == '5':
-        print(f'{drinks_foods_keys[4]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[4]]} 입니다')
-    elif menu == '6':
-        random_drink = random.choice(drinks_foods_keys)
-        print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]} 입니다')
-    elif menu == '7':
-        print(f'다음에 또 오세요')
-        break
+# 8.4
+# e2f = { 'dog' : 'chien', 'cat' : 'chat', 'walrus' : 'morse'}
+# print(e2f['dog'])
+
+# 8.5
+# e2f = { 'dog' : 'chien', 'cat' : 'chat', 'walrus' : 'morse'}
+# for key in e2f:
+#     print(key, end = ' ')
+
+# 8.6
+# life = {'animals' :  {'cats': 'Henri', 'octopi': 'Grumpy', 'emus': 'Lucy'},
+#         'plants':{} ,
+#         'other':{}                                                        }
+
+# 8.7
+# life = {'animals' :  {'cats': 'Henri', 'octopi': 'Grumpy', 'emus': 'Lucy'},
+#         'plants':{} ,
+#         'other':{}                                                        }
+# print(life.keys())
+
+# 8.8
+# life = {'animals' :  {'cats': 'Henri', 'octopi': 'Grumpy', 'emus': 'Lucy'},
+#         'plants':{} ,
+#         'other':{}                                                        }
+# for i in life.keys():
+#         print(i, end=' ')
+#         for j in life[i].keys():
+#                 print(j, end = ' ')
+
+# 8.9
+# life = {'animals' :  {'cats': 'Henri', 'octopi': 'Grumpy', 'emus': 'Lucy'},
+#         'plants':{} ,
+#         'other':{}                                                        }
+# print(life['animals']['cats'])
+
+# 8.10
+# squares = {i : i*i for i in range(10)}
+# print(squares)
+
+# 7.1
+# year_list  = [i for i in range(2001,2007)]
+# print(year_list)
+
+# 7.2
+# year_list  = [i for i in range(2001,2007)]
+# print(year_list[3])
+
+# 7.3
+# year_list  = [i for i in range(2001,2007)]
+# print(year_list.pop())
+# print(year_list[-1])
+
+# 7.4
+# things = ['mozzarella', 'cinderella', 'salmonella']
+
+# 7.5
+# things = ['mozzarella', 'cinderella', 'salmonella']
+# for i in things:
+#     print(i.title())
+# print(things)
+
+# 7.6
+# things = ['mozzarella', 'cinderella', 'salmonella']
+# print(things[0].upper())
+
+# 7.7
+# things = ['mozzarella', 'cinderella', 'salmonella']
+# #things.pop()
+# # del things[2]
+# # things.remove('salmonella')
+# print(things)
+
+# 7.8
+# surprise = ['Groucho', 'Chico', 'Harpo']
+
+# 7.9
+# surprise = ['Groucho', 'Chico', 'Harpo']
+# surprise[2] = surprise[2].lower()
+# print(surprise[2])
+# new = ''
+# for i in surprise[2]:
+#     new = i + new
+# surprise[2] = new.title()
+# print(surprise)
+
+# 7.10
+# list1 = [i for i in range(10) if i%2==0]
+# print(list1)
+
+# 7.11
+# start1 = [ 'fee', 'fie', 'foe']
+# rhymes = [
+#     ('flop', 'get a mop'),
+#     ('fope', 'turn the rope'),
+#     ('fa', 'get your ma'),
+#     ('fudge', 'call the judge'),
+#     ('fat', 'pet the cat'),
+#     ('fog', 'walk hte dog'),
+#     ('fun', "say we're done")
+# ]
+# start2 = 'Someone better'
+#
+#
+# for i in range(len(start1)):
+#     start1[i] = start1[i] + '! '
+#     start1[i] = start1[i].title()
+#
+#
+# for i in range(len(rhymes)):
+#     print(''.join(start1) + rhymes[i][0].title()+'!')
+#     print(start2, rhymes[i][1]  + '.' )
+
+
+
+
+
+
+
+
