4b0a3e40cdef342d175a16bb60ef84bda7c0a097
leyo2001 <leyo2001@naver.com>
2024-01-17 13:34:33 +0900

    v3.5 day04

--- day004.py
+++ day004.py
@@ -1,12 +1,13 @@
-subjects = ["c++", "5", "java", "python", "java", "9", "데이터베이스"]
-#print(subjects[::-1])
-#subjects[::-1]
-# subjects = subjects[::-1] #subjects.reverse()
-# print(subjects)
-# subjects.remove('java')
-# del subjects[1]
-# subjects.pop(1)
-# subjects.sort(reverse=True)
-copy_subjects = sorted(subjects)
-print(subjects)
-print(copy_subjects)
+import copy
+# subjects = ['a', 'b', 'c']
+subjects = ['a', ['b','c'], 'd']
+a = subjects
+b = subjects.copy()
+c = list(subjects)
+d = subjects[:]
+e = copy.deepcopy(a)
+print(subjects, a, b, c, d, e)
+subjects[1][1] = 'x'
+print(subjects, a, b, c, d)
+
+
