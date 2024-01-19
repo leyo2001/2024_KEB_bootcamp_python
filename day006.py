6356d50e6a421e9e26ecda8f23b1bd9b92bb4561
leyo2001 <leyo2001@naver.com>
2024-01-19 15:02:09 +0900

    v5.6 class method resolution order day06

--- day006.py
+++ day006.py
@@ -1,36 +1,33 @@
-class Pokemon:
-    def __init__(self,name):
-        self.name = name
+class Animal:
+    def says(self):
+        return 'I speak'
 
-    def attack(self, target):
-        print(f'{self.name}이(가) {target.name}을(를) 공격!')
-
-
-class Pikachu(Pokemon):
-    def __init__(self, name, type):
-        super().__init__(name)
-        self.type = type
-
-    def attack(self, target):
-        print(f'{self.name}이(가) {target.name}을(를) {self.type} 공격!')
-
-    def electric_info(self):
-        print("전기 계열의 공격을 합니다.")
-
-class Squirtle(Pokemon): # is - a
+class Horse(Animal):
+    # def says(self):
+    #     return 'Neigh!'
     pass
 
-class Agumon:
+class Donkey(Animal):
+    # def says(self):
+    #     return 'Hee Haw!'
+    pass
+class Mule(Donkey, Horse):
     pass
 
-p1 = Pikachu('피카츄','전기')
-p2 = Squirtle('꼬부기')
-p3 = Pokemon("아무개")
-# print(issubclass(Pikachu, Pokemon))
-# print(issubclass(Pokemon, Pikachu))
-print(p1.name)
-p1.attack(p2)
-p1.electric_info()
+class Hinny(Horse, Donkey):
+    pass
+
+
+m1 = Mule()
+h1 = Hinny()
+
+# print(m1.says())
+# print(h1.says())
+print(Hinny.__mro__)
+print(Mule.__mro__)
+
+
+
 
 
 
