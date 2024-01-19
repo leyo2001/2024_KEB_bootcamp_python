918310cff63689ef95ab17ba5300fcbb7a2b5912
leyo2001 <leyo2001@naver.com>
2024-01-19 15:28:37 +0900

    v5.7 class property day06

--- day006.py
+++ day006.py
@@ -1,33 +1,59 @@
-class Animal:
-    def says(self):
-        return 'I speak'
+class FlyingMixin:
+    def fly(self):
+        return f'{self.name}이(가) 하늘을 훨훨 날아갑니다~'
 
-class Horse(Animal):
-    # def says(self):
-    #     return 'Neigh!'
+class SwimmingMinxin:
+    def swim(self):
+        return f'{self.name}이(가) 수영을 합니다'
+
+
+class Pokemon:
+    def __init__(self, name):
+        self.hidden_name = name
+
+    def attack(self):
+        print('공격~')
+
+    def get_name(self):
+        print("inside getter")
+        return self.hidden_name
+
+    def set_name(self, name):
+        print("inside setter")
+        self.hidden_name = name
+
+    name = property(get_name, set_name)
+
+
+
+class Charizard(Pokemon,FlyingMixin):
     pass
 
-class Donkey(Animal):
-    # def says(self):
-    #     return 'Hee Haw!'
-    pass
-class Mule(Donkey, Horse):
+class Gyarados(Pokemon,SwimmingMinxin):
     pass
 
-class Hinny(Horse, Donkey):
-    pass
+g1 = Gyarados("갸라도스")
+c1 = Charizard("리자몽")
+
+# print(g1.swim())
+# print(c1.fly())
+# c1.attack()
+# #Charizard.attack() -> error
+# Charizard.attack(c1)
 
 
-m1 = Mule()
-h1 = Hinny()
+# print(g1.name)
+# g1.name = "잉어킹"
+# print(g1.name)
 
-# print(m1.says())
-# print(h1.says())
-print(Hinny.__mro__)
-print(Mule.__mro__)
-
-
-
+# print(g1.name)
+# print(g1.get_name())
+# g1.set_name('잉어킹')
+# print(g1.get_name())
 
 
+# property
+print(g1.name)
+g1.name = '잉어킹'
+print(g1.name)
 
