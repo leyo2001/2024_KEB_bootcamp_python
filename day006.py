20faa9059b6e89e04844514a10e5d5987f0ec70d
leyo2001 <leyo2001@naver.com>
2024-01-19 13:45:09 +0900

    v5.5 class day06

--- day006.py
+++ day006.py
@@ -1,29 +1,36 @@
-import random
+# class Pokemon:
+#     pass
+#
+#
+# pikachu = Pokemon()
+# squirtle = Pokemon()
+# pikachu.name = '피카츄'
+# pikachu.nemesis = squirtle
+# print(pikachu.name)
+# # print(pikachu.nemesis)
+# pikachu.nemesis.name = '꼬부기' # squirtle.name = '꼬부기'
+# print(pikachu.nemesis.name)
+# print(squirtle.name)
 
 
-class OopsException(Exception):
-    pass
 
-# numbers = list()
-# for i in range(5):
-#     numbers.append(random.randint(1, 100))
-numbers = [random.randint(1, 100) for i in range(10)]
-print(numbers)
 
-try:
-    pick = int(input(f"Input index (0 ~ {len(numbers)-1}) : "))
-    print(numbers[pick])
-    print(5/2)
-    raise OopsException("Oops~~")  # exception!
-except IndexError as err:
-    print(f"Wrong index number\n{err}")
-except ValueError as err:
-    print(f"Input only number~\n{err}")
-except ZeroDivisionError as err:
-    print(f"The denominator cannot be 0.\n{err}")
-# except OopsException as err:
-#     print(f"Oops Oops {err}")
-except Exception as err:
-    print(f"Error occurs : {err}")
-else:
-    print(f"Program terminate")
+
+
+
+
+
+class Pokemon:
+    def __init__(self,name):
+        self.name = name
+        print(f"{name} 포켓몬스터 생성")
+
+    def attack(self, target):
+        print(f'{self.name}이(가) {target.name}을(를) 공격!')
+
+
+charizard = Pokemon('리자몽')
+pikachu = Pokemon('삐까츄')
+squirtle = Pokemon("꼬부기")
+charizard.attack(squirtle)
+
