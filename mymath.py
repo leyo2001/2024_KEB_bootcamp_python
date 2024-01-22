3c3389e633429c57c37fbc0a0d4e14186f9a18c0
leyo2001 <leyo2001@naver.com>
2024-01-22 13:46:15 +0900

    v6.9 module  day07

--- mymath.py
+++ mymath.py
@@ -1,23 +1,28 @@
+
+
+
+
 def isprime(n) -> bool:
-    """
-    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
-    :param n: 판정할 매개변수
-    :return: 소수면 True, 소수가 아니면 False
-    """
+        """
+        매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
+        :param n: 판정할 매개변수
+        :return: 소수면 True, 소수가 아니면 False
+        """
 
-    if n < 2:
-        return False
+        if n < 2:
+            return False
 
-    else:
-        i = 2
-        while i * i <= n:
-            if n % i == 0:
-                return False
-            i += 1
-        return True
+        else:
+            i = 2
+            while i * i <= n:
+                if n % i == 0:
+                    return False
+                i += 1
+            return True
 
 def fahreneit_to_celcius(fahrenheit) -> float:
-    return f'{((fahrenheit-32.0)*5.0/9.0):.4f}C'
+        return f'{((fahrenheit-32.0)*5.0/9.0):.4f}C'
 
 def celcius_to_fahreneit(celsius) -> float:
-    return f'{((celsius*9.0/5.0)+32.0):.4f}F'
+        return f'{((celsius*9.0/5.0)+32.0):.4f}F'
+
