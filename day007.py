b8fc6e4785307e50db75ae8007917de3a71cf4a5
leyo2001 <leyo2001@naver.com>
2024-01-22 11:36:27 +0900

    v6.4 composition  day07

--- day007.py
+++ day007.py
@@ -55,13 +55,13 @@ class Pokemon:
 
 
 class Pikachu(Pokemon):
-    def __init__(self, name, hp, fly):
+    def __init__(self, name, hp):
         self.name = name
         self.hp = hp
-        self.fly_behavior = fly
+        self.fly_behavior = NoFly() #composition
 
-nofly = NoFly()
-p1 = Pikachu('피카츄',35,nofly)
+
+p1 = Pikachu('피카츄',35)
 print(p1.fly_behavior.fly())
 
 
