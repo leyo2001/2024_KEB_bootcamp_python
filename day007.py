c78db0847fc6ea0da026f49b2268f0d9dbe3eb48
leyo2001 <leyo2001@naver.com>
2024-01-22 11:43:37 +0900

    v6.5 module  day07

--- day007.py
+++ day007.py
@@ -1,70 +1,36 @@
-class FlyingBehavior:
-    def fly(self):
-        return f"하늘을 훨훨 날아갑니다~"
-
-class NoFly(FlyingBehavior):
-    def fly(self):
-        return f'하늘을 날 수 없습니다.'
-
-class FlyWithWings(FlyingBehavior):
-    def fly(self):
-        return f'날개로 하늘을 훨훨 날아갑니다'
-
-class JetPack(FlyWithWings):
-    def fly(self):
-        return f'로켓추진기로 하늘을 날아갑니다'
+#module
+import mymath
 
 
+while True:
+    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
 
+    if menu == '1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+    elif menu == '3':
+        number = int(input("Input number : "))
+        if mymath.isprime(number):
+            print(f'{number} is prime number')
+        else:
+            print(f'{number} is NOT prime number!')
+    elif menu == '4':
+        numbers = input("Input first second number : ").split()
+        n1 = int(numbers[0])
+        n2 = int(numbers[1])
 
-class SwimmingBehavior:
-    def swim(self):
-        return f"{self._Pokemon__name}이(가) 수영을 합니다."
+        if n1 > n2:
+            n1, n2 = n2, n1
 
-class Pokemon:
-    def __init__(self, name, hp, fly):
-        self.__name = name
-        self.__hp = hp
-        self.fly_behavior = fly
-
-    def set_fly_behavior(self, fly):
-        self.fly_behavior = fly
-
-
-    def attack(self):
-        print("공격~")
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, new_name):
-        self.__name = new_name
-
-    #name = property(get_name, set_name)
-
-
-    #magic method
-    def __str__(self):
-        return self.__name + " 입니다"
-    def __add__(self, other):
-        #return self.__name + ' + ' + other.__name
-        return f'두 포켓몬스터 체력의합은 {self.__hp + other.__hp}입니다.'
-
-
-
-class Pikachu(Pokemon):
-    def __init__(self, name, hp):
-        self.name = name
-        self.hp = hp
-        self.fly_behavior = NoFly() #composition
-
-
-p1 = Pikachu('피카츄',35)
-print(p1.fly_behavior.fly())
-
-
-
-
-
+        for number in range(n1, n2 + 1):
+            if mymath.isprime(number):
+                print(number, end=' ')
+        print()
+    elif menu == '5':
+        print('Terminate Program.')
+        break
+    else:
+        print('Invalid Menu!')
